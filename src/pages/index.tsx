867121e0d0ff6c4ebecd1539041e067e648f19c1
Higor Abreu <higorabreuf@gmail.com>
2023-09-06 02:52:04 -0300

    project config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss"
+
+const HomeNotAuth = function () {
+  return (		
+  <>
+    <Head>Home</Head>
+    <main></main>
+  </>
+)};
+
+export default HomeNotAuth;
