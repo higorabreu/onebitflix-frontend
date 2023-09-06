ca2c630776a9fb30fda1825f51208974628498f0
Higor Abreu <higorabreuf@gmail.com>
2023-09-06 03:46:49 -0300

    header HomeNoAuth

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss" 
+import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
+
+const HomeNoAuth = function () {
+  return (
+      <>
+        <Head>
+          <title>Onebitflix</title>
+          <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+          <meta property="og:title" content="Onebitflix" key="title" />
+          <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
+        </Head>
+        <main>
+          <HeaderNoAuth/>
+        </main>
+      </>
+);
+};
+
+export default HomeNoAuth;
